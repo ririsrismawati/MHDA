1877de575cc7911492ea06450e07620bea5fce3c
Riris Rismawati <ririsrismawati@Ririss-MacBook-Pro.local>
2017-07-07 00:43:20 +0700

    update card, flow, and design

--- app/src/main/java/com/sttb/mobileprogramming/mengenalhurufdanangka/CardActivity.java
+++ app/src/main/java/com/sttb/mobileprogramming/mengenalhurufdanangka/CardActivity.java
@@ -1,117 +1,205 @@
 package com.sttb.mobileprogramming.mengenalhurufdanangka;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import com.daprlabs.aaron.swipedeck.SwipeDeck;
-
 import android.content.Context;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.daprlabs.cardstack.SwipeDeck;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class CardActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
     private SwipeDeck cardStack;
     private Context context = this;
+
     private SwipeDeckAdapter adapter;
-    private ArrayList<String> testData;
-    private CheckBox dragCheckbox;
+    private ArrayList<String> titleData;
+    private ArrayList<String> subTitleData;
+    private int[] imageData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
         cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
-        dragCheckbox = (CheckBox) findViewById(R.id.checkbox_drag);
+        cardStack.setHardwareAccelerationEnabled(true);
 
-        testData = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            testData.add(String.valueOf(i));
+        setArrayData();
+
+        adapter = new SwipeDeckAdapter(titleData, subTitleData, imageData, this);
+        cardStack.setAdapter(adapter);
+
+        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
+            @Override
+            public void cardSwipedLeft(int position) {
+                addDataWithPosition(position);
+            }
+
+            @Override
+            public void cardSwipedRight(int position) {
+                addDataWithPosition(position);
+            }
+
+            @Override
+            public void cardsDepleted() {
+            }
+
+            @Override
+            public void cardActionDown() {
+            }
+
+            @Override
+            public void cardActionUp() {
+            }
+
+        });
+    }
+
+    private void addDataWithPosition(int position) {
+        if (position == titleData.size() - 1) {
+            addData();
         }
+    }
 
-        adapter = new SwipeDeckAdapter(testData, this);
-        if(cardStack != null){
-            cardStack.setAdapter(adapter);
-        }
-        cardStack.setCallback(new SwipeDeck.SwipeDeckCallback() {
-            @Override
-            public void cardSwipedLeft(long stableId) {
-                Log.i("MainActivity", "card was swiped left, position in adapter: " + stableId);
-            }
+    private void addData() {
+        setArrayData();
 
-            @Override
-            public void cardSwipedRight(long stableId) {
-                Log.i("MainActivity", "card was swiped right, position in adapter: " + stableId);
+        SwipeDeckAdapter adapter = new SwipeDeckAdapter(titleData, subTitleData, imageData, context);
+        cardStack.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
 
-            }
+    private void setArrayData() {
+        titleData = new ArrayList<>();
+        titleData.add("A a");
+        titleData.add("B b");
+        titleData.add("C c");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
+        titleData.add(" ");
 
-            @Override
-            public boolean isDragEnabled(long itemId) {
-                return dragCheckbox.isChecked();
-            }
-        });
 
-        cardStack.setLeftImage(R.id.left_image);
-        cardStack.setRightImage(R.id.right_image);
+        subTitleData = new ArrayList<>();
+        subTitleData.add("Ayam");
+        subTitleData.add("Bola");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
+        subTitleData.add("");
 
-        Button btn = (Button) findViewById(R.id.button_left);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardStack.swipeTopCardLeft(500);
+        imageData = new int[]{
+                R.drawable.ayam,
+                R.drawable.bola,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+                R.drawable.,
+        };
+    }
 
-            }
-        });
-        Button btn2 = (Button) findViewById(R.id.button_right);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cardStack.swipeTopCardRight(180);
-            }
-        });
 
-        Button btn3 = (Button) findViewById(R.id.button_center);
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                testData.add("a sample string.");
-//                adapter.notifyDataSetChanged();
-                cardStack.unSwipeCard();
-            }
-        });
-
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
     public class SwipeDeckAdapter extends BaseAdapter {
 
-        private List<String> data;
+        private List<String> titleList;
+        private List<String> subTitleList;
+        private int[] imageList;
         private Context context;
+        private TextToSpeech tts;
 
-        public SwipeDeckAdapter(List<String> data, Context context) {
-            this.data = data;
+
+        public SwipeDeckAdapter(List<String> titleList, List<String> subTitleList, int[] imageList, Context context) {
+            this.titleList = titleList;
+            this.subTitleList = subTitleList;
+            this.imageList = imageList;
             this.context = context;
         }
 
         @Override
         public int getCount() {
-            return data.size();
+            return titleList.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return data.get(position);
+            return titleList.get(position);
         }
+
 
         @Override
         public long getItemId(int position) {
@@ -124,27 +212,49 @@ public class CardActivity extends AppCompatActivity {
             View v = convertView;
             if (v == null) {
                 LayoutInflater inflater = getLayoutInflater();
-                // normally use a viewholder
-                v = inflater.inflate(R.layout.test_card2, parent, false);
+                v = inflater.inflate(R.layout.card, parent, false);
             }
-            //((TextView) v.findViewById(R.id.textView2)).setText(data.get(position));
-            ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
-            Picasso.with(context).load(R.drawable.food).fit().centerCrop().into(imageView);
-            TextView textView = (TextView) v.findViewById(R.id.sample_text);
-            String item = (String)getItem(position);
-            textView.setText(item);
 
-            v.setOnClickListener(new View.OnClickListener() {
+            ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
+            ImageView buttonPlay = (ImageView) v.findViewById(R.id.btn_play);
+            Picasso.with(context).load(imageList[position]).fit().centerCrop().into(imageView);
+            TextView tvTitle = (TextView) v.findViewById(R.id.title);
+            TextView tvSubtitle = (TextView) v.findViewById(R.id.subtitle);
+            final String title = (String) titleList.get(position);
+            final String subTitle = (String) subTitleList.get(position);
+            tvTitle.setText(title);
+            tvSubtitle.setText(subTitle);
+
+            buttonPlay.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Log.i("Layer type: ", Integer.toString(v.getLayerType()));
-                    Log.i("Hardware Accel type:", Integer.toString(View.LAYER_TYPE_HARDWARE));
-                    /*Intent i = new Intent(v.getContext(), BlankActivity.class);
-                    v.getContext().startActivity(i);*/
+                    tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+
+                        @Override
+                        public void onInit(int status) {
+                            // TODO Auto-generated method stub
+                            if (status == TextToSpeech.SUCCESS) {
+                                tts.setLanguage(new Locale("id", "ID"));
+                                speakOut(removeLastChar(title));
+                            }
+                        }
+                    });
+
                 }
             });
+
             return v;
         }
-    }
-}
 
+        private String removeLastChar(String str) {
+            return str.substring(0, str.length() - 1);
+        }
+
+        private void speakOut(String value) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "UniqueID");
+            tts.speak(value, TextToSpeech.QUEUE_FLUSH, map);
+        }
+    }
+
+}
