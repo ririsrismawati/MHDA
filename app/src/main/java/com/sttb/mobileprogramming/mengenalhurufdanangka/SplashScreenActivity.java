db7f88c62c74231bc6c705e0e4833cc91dbd84b6
Riris Rismawati <ririsrismawati@Ririss-MacBook-Pro.local>
2017-07-07 16:38:46 +0700

    update splashscreen

--- app/src/main/java/com/sttb/mobileprogramming/mengenalhurufdanangka/SplashScreenActivity.java
+++ app/src/main/java/com/sttb/mobileprogramming/mengenalhurufdanangka/SplashScreenActivity.java
@@ -13,7 +13,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     /**
      * Duration of wait
      **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 7000;
 
     /**
      * Called when the activity is first created.
